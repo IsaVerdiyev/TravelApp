b8abc8dd700db96f2f564ea1771d28609e80fa6a
IsaVerdiyev <verdiyev.isa1992@gmail.com>
2019-01-09 00:31:01 +0400

    Added new project for Infrastructure and created context for working with database. Also implemented generic repository interface in this layer

---
+++ TravelAppInfrastructure/Data/EfRepository.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAppCore.Entities;
+using TravelAppCore.Interfaces;
+
+namespace TravelAppInfrastructure.Data
+{
+    class EfRepository<T> : IRepository<T> where T : BaseEntity
+    {
+
+        TripDb tripDb;
+
+        public EfRepository(TripDb tripDb)
+        {
+            this.tripDb = tripDb;
+        }
+
+        public T Add(T entity)
+        {
+            T added = tripDb.Set<T>().Add(entity);
+            tripDb.SaveChanges();
+            return added;
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            T added = tripDb.Set<T>().Add(entity);
+            await tripDb.SaveChangesAsync();
+            return added;
+        }
+
+        public void Delete(T entity)
+        {
+            tripDb.Set<T>().Remove(entity);
+            tripDb.SaveChanges();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            tripDb.Set<T>().Remove(entity);
+            await tripDb.SaveChangesAsync();
+        }
+
+        public T GetById(int id)
+        {
+            return tripDb.Set<T>().Find(id);
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return  await tripDb.Set<T>().FindAsync(id);
+        }
+
+        public IEnumerable<T> ListAll()
+        {
+            return tripDb.Set<T>().AsNoTracking().ToList();
+        }
+
+        public async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await tripDb.Set<T>().AsNoTracking().ToListAsync();
+        }
+
+        public void Update(T entity)
+        {
+            tripDb.Entry<T>(entity).State = EntityState.Modified;
+            tripDb.SaveChanges();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            tripDb.Entry<T>(entity).State = EntityState.Modified;
+            await tripDb.SaveChangesAsync();
+        }
+    }
+}
---
+++ TravelAppInfrastructure/Data/TripDb.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAppCore.Entities;
+
+namespace TravelAppInfrastructure.Data
+{
+    class TripDb: DbContext
+    {
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<CityCoordinate> CityCoordinates { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<Trip> Trips { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public TripDb(): base("name=TripDbConnection")
+        {
+
+        }
+    }
+}
