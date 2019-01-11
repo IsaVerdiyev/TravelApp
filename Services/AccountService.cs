e8a70218965177d193eff26d20038e37fc4e95bb
IsaVerdiyev <verdiyev.isa1992@gmail.com>
2019-01-12 03:59:09 +0400

    Added new specification for searching user by nickname or email address and also added service of IAccount service but yet didn't implemented it totally

---
+++ Services/AccountService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using TravelAppCore.Entities;
+using TravelAppCore.Interfaces;
+using TravelAppCore.Specifications;
+
+namespace Services
+{
+    public class AccountService : IAccountService
+    {
+        IRepository<User> userRepos;
+
+        string salt = "sdflkj;adsf";
+
+        public AccountService(IRepository<User> userRepos)
+        {
+            this.userRepos = userRepos;
+        }
+
+        public bool TryLogIn(string nick, string password, out User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> TryLogInAsync(string nick, string password, out User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TrySignUp(ref User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> TrySignUpAsync(ref User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TravelAppCore/Specifications/UserByNickOrEmailSpecification.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TravelAppCore.Entities;
+
+namespace TravelAppCore.Specifications
+{
+    public class UserByNickOrEmailSpecification: BaseSpecification<User>
+    {
+        public UserByNickOrEmailSpecification(string nickOrEmail): base(u => string.Compare(u.NickName, nickOrEmail) == 0 || string.Compare(u.Email, nickOrEmail) == 0)
+        {
+
+        }
+    }
+}
