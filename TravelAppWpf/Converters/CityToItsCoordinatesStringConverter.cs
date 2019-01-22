e6c74a1fa47a56baf844822419caf0feaefe98ef
IsaVerdiyev <verdiyev.isa1992@gmail.com>
2019-01-22 21:42:50 +0400

    Added some code for view of CityOnMapView and for its viewmodel. Center property of map in view doesn't work yet

---
+++ TravelAppWpf/Converters/CityToItsCoordinatesStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using TravelAppCore.Entities;
+
+namespace TravelAppWpf.Converters
+{
+    class CityToItsCoordinatesStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            City city = value as City;
+            return $"{city?.CityCoordinate.Latitude.ToString()}, {city?.CityCoordinate.Longitude.ToString()}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TravelAppWpf/ViewModels/CityOnMapViewModel.cs
+++ TravelAppWpf/ViewModels/CityOnMapViewModel.cs
@@ -1,13 +1,61 @@
 ﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TravelAppCore.Entities;
+using TravelAppWpf.Messages;
+using TravelAppWpf.Navigation;
 
 namespace TravelAppWpf.ViewModels
 {
     class CityOnMapViewModel: ViewModelBase
     {
+
+        #region Fields And Properties
+
+        User user;
+        Trip trip;
+
+        City city;
+
+        public City City { get => city; set => Set(ref city, value); }
+
+        //public string LatLob { get => $"{city}"}
+
+        #endregion
+
+        #region Dependencies
+        private readonly INavigator navigator;
+        #endregion
+
+        #region Constructors
+
+        public CityOnMapViewModel(INavigator navigator)
+        {
+            this.navigator = navigator;
+
+            Messenger.Default.Register<CityOnMapViewModelMessage>(this, m => {
+                user = m.User;
+                trip = m.Trip;
+                City = m.City;
+            });
+        }
+
+        #endregion
+
+
+        #region Commands
+
+        RelayCommand returnBackCommand;
+        public RelayCommand ReturnBackCommand
+        {
+            get => returnBackCommand ?? (returnBackCommand = new RelayCommand(() => navigator.NavigateTo<CitiesViewModel>()));
+        }
+
+        #endregion
     }
 }
