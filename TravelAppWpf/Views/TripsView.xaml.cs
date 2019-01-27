e3584ad3d71a018653771c6b5c74e34e7cb6b631
IsaVerdiyev <verdiyev.isa1992@gmail.com>
2019-01-27 21:18:31 +0400

    Added logic of disable buttons when trip is deleted in TripsView.

--- TravelAppWpf/Views/TripsView.xaml.cs
+++ TravelAppWpf/Views/TripsView.xaml.cs
@@ -24,5 +24,10 @@ namespace TravelAppWpf.Views
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            (sender as Button).IsEnabled = false;
+        }
     }
 }
