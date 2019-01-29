91d1e2a4e24536cfdf3be5d35438b1399637930f
IsaVerdiyev <verdiyev.isa1992@gmail.com>
2019-01-29 19:42:54 +0400

    In same way as with AddTripViewModel injected IProcessesInfoService in AddTicketViewModel and adjusted its view to use this new feature.

--- TravelAppWpf/ViewModels/AddTicketViewModel.cs
+++ TravelAppWpf/ViewModels/AddTicketViewModel.cs
@@ -9,19 +9,22 @@ using System.Threading.Tasks;
 using System.Windows;
 using TravelAppCore.Entities;
 using TravelAppCore.Interfaces;
+using TravelAppWpf.Extensions;
 using TravelAppWpf.Messages;
 using TravelAppWpf.Navigation;
+using TravelAppWpf.Services.ProcessesInfo;
 
 namespace TravelAppWpf.ViewModels
 {
-    class AddTicketViewModel: ViewModelBase
+    class AddTicketViewModel : ViewModelBase
     {
         #region Fields And Properties
 
         Trip trip;
 
         string ticketName;
-        public string TicketName {
+        public string TicketName
+        {
             get => ticketName;
             set
             {
@@ -31,7 +34,8 @@ namespace TravelAppWpf.ViewModels
         }
 
         string pdfPath;
-        public string PdfPath {
+        public string PdfPath
+        {
             get => pdfPath;
             set
             {
@@ -40,29 +44,45 @@ namespace TravelAppWpf.ViewModels
             }
         }
 
+        private string currentProcessesInfo;
+        public string CurrentProcessesInfo
+        {
+            get { return currentProcessesInfo; }
+            set
+            {
+                Set(ref currentProcessesInfo, value);
+
+            }
+        }
+
         #endregion
 
         #region Messages
 
         UpdateTicketsMessage updateTicketsMessage = new UpdateTicketsMessage();
-        private readonly INavigator navigator;
+        UpdateProcessInfoMessage updateProcessInfoMessage = new UpdateProcessInfoMessage();
 
         #endregion
 
         #region Dependencies
 
-        ITicketService ticketService;
+        private readonly INavigator navigator;
+        private readonly IProcessesInfoService processesInfoService;
+        private readonly ITicketService ticketService;
 
         #endregion
 
         #region Constructors
 
-        public AddTicketViewModel(INavigator navigator, ITicketService ticketService)
+        public AddTicketViewModel(INavigator navigator, IProcessesInfoService processesInfoService, ITicketService ticketService)
         {
             this.navigator = navigator;
+            this.processesInfoService = processesInfoService;
             this.ticketService = ticketService;
 
             Messenger.Default.Register<AddTicketViewModelMessage>(this, m => trip = m.Trip);
+
+            Messenger.Default.Register<UpdateProcessInfoMessage>(this, m => UpdateCurrentProcessesInfo());
         }
 
         #endregion
@@ -75,12 +95,26 @@ namespace TravelAppWpf.ViewModels
         {
             get => addTicketCommand ?? (
                 addTicketCommand = new RelayCommand(
-                    async () => {
-                        await Task.Run(async() => {
-                            await ticketService.AddTicketAsync(trip, new Ticket {Name = TicketName,ImagePath = PdfPath});
-                            navigator.NavigateTo<TicketsViewModel>();
-                            Messenger.Default.Send<UpdateTicketsMessage>(updateTicketsMessage);
-                        });
+                    async () =>
+                    {
+
+                        int processId = processesInfoService.GenerateUniqueId();
+                        processesInfoService.ActivateProcess(ProcessEnum.AddingTicket, processesInfoService.ProcessNames[ProcessEnum.AddingTicket], processId);
+                        try
+                        {
+                            Messenger.Default.Send<UpdateProcessInfoMessage>(updateProcessInfoMessage);
+                            await Task.Run(async () =>
+                            {
+                                await ticketService.AddTicketAsync(trip, new Ticket { Name = TicketName, ImagePath = PdfPath });
+                                navigator.NavigateTo<TicketsViewModel>();
+                                Messenger.Default.Send<UpdateTicketsMessage>(updateTicketsMessage);
+                            });
+                        }
+                        finally
+                        {
+                            processesInfoService.DeactivateProcess(ProcessEnum.AddingTicket, processId);
+                            Messenger.Default.Send<UpdateProcessInfoMessage>(updateProcessInfoMessage);
+                        }
                     },
                     () => !string.IsNullOrWhiteSpace(TicketName) && !string.IsNullOrWhiteSpace(PdfPath))
                 );
@@ -92,6 +126,22 @@ namespace TravelAppWpf.ViewModels
         {
             get => returnCommand ?? (returnCommand = new RelayCommand(() => navigator.NavigateTo<TicketsViewModel>()));
         }
+        #endregion
+
+        #region Private Functions
+
+        void UpdateCurrentProcessesInfo()
+        {
+            try
+            {
+                CurrentProcessesInfo = processesInfoService.GetOneInfoStringFromAllProcesses();
+            }
+            catch (InvalidOperationException ex)
+            {
+                CurrentProcessesInfo = "";
+            }
+        }
+
         #endregion
     }
 }
